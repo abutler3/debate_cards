7a3346e653d5ad6aa2aea47a0cd94b3ed77064e9
Drew Butler <abutler3@gmail.com>
2012-11-21 11:24:04 -0600

    Fix year search in search form

--- app/models/search.rb
+++ app/models/search.rb
@@ -12,7 +12,7 @@ private
     cards = cards.where("evidence @@ ?", keywords) if keywords.present?
     cards = cards.where("tag @@ ?", tag) if tag.present?
     cards = cards.where("author @@ ?", author) if author.present?
-    cards = cards.where("date @@ ?", year) if year.present?
+    cards = cards.where("date like ?", "%#{year}%") if year.present?
     cards
   end
 end
