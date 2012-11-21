31f3749b064cdbb32745521cf80cb4606d621d77
Drew Butler <abutler3@gmail.com>
2012-11-21 12:16:49 -0600

    Moved year search field back to SQL because @@ was calling a string instead of an integer

--- app/models/search.rb
+++ app/models/search.rb
@@ -13,6 +13,7 @@ private
     cards = cards.where("tag @@ ?", tag) if tag.present?
     cards = cards.where("author @@ ?", author) if author.present?
     cards = cards.where("date like ?", "%#{year}%") if year.present?
+    # Fix through http://stackoverflow.com/questions/8795086/rails-3-1-heroku-pgerror-operator-does-not-exist-character-varying-integer
     cards
   end
 end
